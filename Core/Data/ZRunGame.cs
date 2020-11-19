3921815a069f5d64fe923869b34ab4d95f198996
Will Wouod <dmitrievsvp@gmail.com>
2020-11-19 13:40:31 +0200

    Pipe reader code extracted into a new class

--- Core/Data/ZRunGame.cs
+++ Core/Data/ZRunGame.cs
@@ -1,17 +1,12 @@
 ﻿using System;
 using System.Diagnostics;
-using System.IO;
-using System.IO.Pipes;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 using Zlo4NET.Api.Models.Shared;
 using Zlo4NET.Api.Service;
 using Zlo4NET.Core.Data.Parsers;
-using Zlo4NET.Core.Extensions;
 using Zlo4NET.Core.Helpers;
 using Zlo4NET.Core.Services;
 using Zlo4NET.Core.ZClient.Data;
@@ -21,18 +16,13 @@ namespace Zlo4NET.Core.Data
 {
     internal class ZRunGame : IZRunGame
     {
-        private const int _messageHeaderSize = 4;
-
         private readonly IZClientService _clientService;
         private readonly IZGameRunParser _parser;
-        private readonly Thread _pipeReadThread;
-        private readonly NamedPipeClientStream _pipeClient;
         private readonly IZProcessTracker _processTracker;
         private readonly ZInstalledGame _targetGame;
         private readonly string _runArgs;
         private readonly ZLogger _logger;
-
-        private readonly _Buffer _dynamicBuffer;
+        private readonly _GamePipe _pipe;
 
         public ZRunGame(
             IZClientService clientService,
@@ -47,14 +37,18 @@ namespace Zlo4NET.Core.Data
             _targetGame = targetGame;
             _logger = ZLogger.Instance;
 
-            _pipeReadThread = new Thread(_ReadPipeMethod) { IsBackground = true };
-            _pipeClient = new NamedPipeClientStream(".", pipeName, PipeDirection.In);
+            _pipe = new _GamePipe(_logger, pipeName);
             _processTracker = new ZProcessTracker(processName, TimeSpan.FromSeconds(1), false, processes => processes.First());
+
+            _pipe.PipeEvent += _PipeEventHandler;
 
             _processTracker.ProcessDetected += _ProcessTrackerOnProcessDetected;
             _processTracker.ProcessLost += _ProcessTrackerOnProcessLost;
+        }
 
-            _dynamicBuffer = new _Buffer();
+        private void _PipeEventHandler(_GameState state)
+        {
+            // TODO: Handle pipe here
         }
 
         public ZRunGame(string processName)
@@ -120,86 +114,15 @@ namespace Zlo4NET.Core.Data
 
         #region Private methods
 
-        private void _ReadPipeMethod()
-        {
-            _pipeClient.Connect();
-
-            // read data from pipe
-            while (_pipeClient.IsConnected && _pipeClient.CanRead)
-            {
-                var buffer = new byte[4096];
-                var bytesRead = _pipeClient.Read(buffer, 0, buffer.Length);
-
-                if (bytesRead > 0)
-                {
-                    // create message data block
-                    var messageData = buffer.Take(bytesRead);
-
-                    // append message data
-                    _dynamicBuffer.Append(messageData);
-
-                    // parse message
-                    _parseData();
-                }
-
-                Thread.Sleep(50); // wait to data availability
-            }
-        }
-
-        private void _parseData()
-        {
-            var eventString = string.Empty;
-            var stateString = string.Empty;
-
-            try
-            {
-                using (var memoryStream = new MemoryStream(_dynamicBuffer.BufferData, false))
-                using (var br = new BinaryReader(memoryStream, Encoding.ASCII))
-                {
-                    br.ReadBytes(2); // skip two unknown bytes Approved by ZLOFENIX
-                    
-                    // read message length
-                    var messageLength = br.ReadUInt16();
-
-                    // check, we got the full message or not
-                    if (_dynamicBuffer.Size < messageLength - _messageHeaderSize)
-                    {
-                        return;
-                    }
-
-                    // read and parse message data
-                    var eventStringLength = br.ReadByte();
-
-                    eventString = br.ReadCountedString(eventStringLength)
-                        .Trim();
-
-                    var stateStringLength = br.ReadUInt16();
-
-                    stateString = br.ReadCountedString(stateStringLength)
-                        .Trim();
-
-                    //    .Replace('\0'.ToString(), string.Empty);
-                    //secondPartString = Uri.UnescapeDataString(secondPartString);
-
-                    // normalize state string
-                    stateString = Uri.UnescapeDataString(stateString);
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.Error($"{nameof(_parseData)} message {ex.Message}");
-            }
-
-            _dynamicBuffer.Clear();
-            _onMessage(eventString, stateString);
-        }
-
         private void _ProcessTrackerOnProcessLost(object sender, EventArgs e)
         {
             _processTracker.ProcessDetected -= _ProcessTrackerOnProcessDetected;
             _processTracker.ProcessLost -= _ProcessTrackerOnProcessLost;
 
             _onMessage("StateChanged", "State_GameClose");
+
+            _pipe.PipeEvent -= _PipeEventHandler;
+            _pipe.Stop();
         }
 
         private void _ProcessTrackerOnProcessDetected(object sender, Process e)
@@ -207,7 +130,7 @@ namespace Zlo4NET.Core.Data
             _onMessage("StateChanged", "State_GameRun");
 
             // ? cuz we cannot always create an instance
-            _pipeReadThread?.Start();
+            _pipe?.Begin();
         }
 
         private void _onMessage(string firstPart, string secondPart)
