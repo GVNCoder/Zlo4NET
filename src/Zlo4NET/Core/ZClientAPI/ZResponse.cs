5d08efad6c79e40d4246dc36118a9e07ccc86924
DmitrievStanislav <dmitrievsvp@gmail.com>
2021-04-18 22:40:18 +0300

    Implemented ZResponse class

---
+++ src/Zlo4NET/Core/ZClientAPI/ZResponse.cs
@@ -0,0 +1,21 @@
+﻿namespace Zlo4NET.Core.ZClientAPI
+{
+    /// <summary>
+    /// Represents the unit of response from ZClient
+    /// </summary>
+    internal class ZResponse
+    {
+        /// <summary>
+        /// Gets or sets response status code
+        /// </summary>
+        public ZResponseStatusCode StatusCode { get; set; }
+        /// <summary>
+        /// Gets or sets response packets
+        /// </summary>
+        public ZPacket[] ResponsePackets { get; set; }
+        /// <summary>
+        /// Gets or sets request instance
+        /// </summary>
+        public ZRequest Request { get; set; }
+    }
+}
---
+++ src/Zlo4NET/Core/ZClientAPI/ZResponseStatusCode.cs
@@ -0,0 +1,25 @@
+﻿namespace Zlo4NET.Core.ZClientAPI
+{
+    /// <summary>
+    /// Represents the response status codes
+    /// </summary>
+    internal enum ZResponseStatusCode
+    {
+        /// <summary>
+        /// Occurs when a request was got a associated response
+        /// </summary>
+        Ok,
+        /// <summary>
+        /// Occurs when a request was got timeout
+        /// </summary>
+        Timeout, // Task.First(requestTask, timeoutTask)
+        /// <summary>
+        /// Occurs when a request was denied while trying to send it
+        /// </summary>
+        Declined,
+        /// <summary>
+        /// Occurs when a request was rejected from processing queue
+        /// </summary>
+        Rejected
+    }
+}
