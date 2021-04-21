e44404ed16aa58ca8f72941be56094d304fc2041
DmitrievStanislav <dmitrievsvp@gmail.com>
2021-04-21 21:42:36 +0300

    Removed redundant
    * Added event args class for a new event in IZConnection

---
+++ src/Zlo4NET/Api/Models/Shared/ZAuthorizedEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Zlo4NET.Api.Service;
+
+namespace Zlo4NET.Api.Models.Shared
+{
+    /// <summary>
+    /// Represents <see cref="IZConnection.Authorized"/> event args
+    /// </summary>
+    public class ZAuthorizedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Authorized in ZClient user
+        /// </summary>
+        public ZUser AuthorizedUser { get; }
+        /// <summary>
+        /// Creates an instance of <see cref="ZAuthorizedEventArgs"/>
+        /// </summary>
+        public ZAuthorizedEventArgs(ZUser user)
+        {
+            AuthorizedUser = user;
+        }
+    }
+}
