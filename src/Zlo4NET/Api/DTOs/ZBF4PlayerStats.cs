f06220c7b5bf4a7acaff663230cef4d22b353d48
DmitrievStanislav <dmitrievsvp@gmail.com>
2021-05-29 16:54:38 +0300

    Update ZBF4PlayerStats.cs

--- src/Zlo4NET/Api/DTOs/ZBF4PlayerStats.cs
+++ src/Zlo4NET/Api/DTOs/ZBF4PlayerStats.cs
@@ -21,103 +21,101 @@ namespace Zlo4NET.Api.DTOs
 
         #region Auto-mapped properties
 
-        [ZStatsMapper(MapFromDictionaryKey = "rank")]
-        public float Rank { get; set; }
+        [ZStatsMapper(MapFromDictionaryKey = "rank", TargetType = typeof(int))]
+        public int Rank { get; set; }
         [ZStatsMapper(MapFromDictionaryKey = "sc_rank")]
-        public float RankCurrentScore { get; set; }
+        public double RankCurrentLongScore { get; set; }
         [ZStatsMapper(MapFromDictionaryKey = "skill")]
         public float Skill { get; set; }
-        [ZStatsMapper(MapFromDictionaryKey = "c___k_g")]
-        public float Kills { get; set; }
-        [ZStatsMapper(MapFromDictionaryKey = "c___d_g")]
-        public float Deaths { get; set; }
-        [ZStatsMapper(MapFromDictionaryKey = "c_mwin__roo_g")]
-        public float Wins { get; set; }
-        [ZStatsMapper(MapFromDictionaryKey = "c_mlos__roo_g")]
-        public float Losses { get; set; }
-        [ZStatsMapper(MapFromDictionaryKey = "c___sfw_g")]
-        public float Shots { get; set; }
-        [ZStatsMapper(MapFromDictionaryKey = "c___shw_g")]
-        public float Hits { get; set; }
-        [ZStatsMapper(MapFromDictionaryKey = "c___hsh_g")]
-        public float Headshots { get; set; }
+        [ZStatsMapper(MapFromDictionaryKey = "c___k_g", TargetType = typeof(int))]
+        public int Kills { get; set; }
+        [ZStatsMapper(MapFromDictionaryKey = "c___d_g", TargetType = typeof(int))]
+        public int Deaths { get; set; }
+        [ZStatsMapper(MapFromDictionaryKey = "c_mwin__roo_g", TargetType = typeof(int))]
+        public int Wins { get; set; }
+        [ZStatsMapper(MapFromDictionaryKey = "c_mlos__roo_g", TargetType = typeof(int))]
+        public int Losses { get; set; }
+        [ZStatsMapper(MapFromDictionaryKey = "c___sfw_g", TargetType = typeof(int))]
+        public int Shots { get; set; }
+        [ZStatsMapper(MapFromDictionaryKey = "c___shw_g", TargetType = typeof(int))]
+        public int Hits { get; set; }
+        [ZStatsMapper(MapFromDictionaryKey = "c___hsh_g", TargetType = typeof(int))]
+        public int Headshots { get; set; }
         [ZStatsMapper(MapFromDictionaryKey = "c___hsd_ghva")]
         public float LongestHeadshot { get; set; }
         [ZStatsMapper(MapFromDictionaryKey = "c___sa_g")]
         public float TimeSeconds { get; set; }
-        //[ZStatsMapper(MapFromDictionaryKey = "elo_games")]
-        //public float Rounds { get; set; }
-        [ZStatsMapper(MapFromDictionaryKey = "c_vA__de_g")]
-        public float VehicleDestroyed { get; set; }
-        [ZStatsMapper(MapFromDictionaryKey = "c___dt_g")]
-        public float Dogtags { get; set; }
-        [ZStatsMapper(MapFromDictionaryKey = "c___nk_ghva")]
-        public float NemesisStreak { get; set; }
-        [ZStatsMapper(MapFromDictionaryKey = "c___k_ghvs")]
-        public float HighestKillStreak { get; set; }
+        [ZStatsMapper(MapFromDictionaryKey = "c_vA__de_g", TargetType = typeof(int))]
+        public int VehicleDestroyed { get; set; }
+        [ZStatsMapper(MapFromDictionaryKey = "c___dt_g", TargetType = typeof(int))]
+        public int Dogtags { get; set; }
+        [ZStatsMapper(MapFromDictionaryKey = "c___nk_ghva", TargetType = typeof(int))]
+        public int NemesisStreak { get; set; }
+        [ZStatsMapper(MapFromDictionaryKey = "c___k_ghvs", TargetType = typeof(int))]
+        public int HighestKillStreak { get; set; }
 
         // Kits
 
         [ZStatsMapper(MapFromDictionaryKey = "sc_assault")]
-        public float AssaultCurrentScore { get; set; }
-        [ZStatsMapper(MapFromDictionaryKey = "ssclbas_00")]
-        public float AssaultStartCount { get; set; }
+        public double AssaultCurrentLongScore { get; set; }
+        [ZStatsMapper(MapFromDictionaryKey = "ssclbas_00", TargetType = typeof(int))]
+        public int AssaultStartCount { get; set; }
 
         [ZStatsMapper(MapFromDictionaryKey = "sc_engineer")]
-        public float EngineerCurrentScore { get; set; }
-        [ZStatsMapper(MapFromDictionaryKey = "ssclbe_00")]
-        public float EngineerStartCount { get; set; }
+        public double EngineerCurrentLongScore { get; set; }
+        [ZStatsMapper(MapFromDictionaryKey = "ssclbe_00", TargetType = typeof(int))]
+        public int EngineerStartCount { get; set; }
 
         [ZStatsMapper(MapFromDictionaryKey = "sc_recon")]
-        public float ReconCurrentScore { get; set; }
-        [ZStatsMapper(MapFromDictionaryKey = "ssclbr_00")]
-        public float ReconStartCount { get; set; }
+        public double ReconCurrentLongScore { get; set; }
+        [ZStatsMapper(MapFromDictionaryKey = "ssclbr_00", TargetType = typeof(int))]
+        public int ReconStartCount { get; set; }
 
         [ZStatsMapper(MapFromDictionaryKey = "sc_support")]
-        public float SupportCurrentScore { get; set; }
-        [ZStatsMapper(MapFromDictionaryKey = "ssclbsu_00")]
-        public float SupportStartCount { get; set; }
+        public double SupportCurrentLongScore { get; set; }
+        [ZStatsMapper(MapFromDictionaryKey = "ssclbsu_00", TargetType = typeof(int))]
+        public int SupportStartCount { get; set; }
 
         [ZStatsMapper(MapFromDictionaryKey = "sc_commander")]
-        public float CommanderCurrentScore { get; set; }
-        [ZStatsMapper(MapFromDictionaryKey = "ssclco_00")]
-        public float CommanderStartCount { get; set; }
+        public double CommanderCurrentLongScore { get; set; }
+        [ZStatsMapper(MapFromDictionaryKey = "ssclco_00", TargetType = typeof(int))]
+        public int CommanderStartCount { get; set; }
 
         #endregion
 
         #region Calculated properies
 
-        //public double RankShortScore { get; set; }
+        public string RankName { get; set; }
+        public double RankMaxRelativeScore { get; set; }
+        public double RankMaxLongScore { get; set; }
+        public double RankCurrentRelativeScore { get; set; }
         public double ScoreToRankUp { get; set; }
         public float ScoreToRankUpPercent { get; set; }
         public float Accuracy { get; set; }
         public float WL { get; set; }
         public float KD { get; set; }
+        public float AssaultCurrentRelativeScore { get; set; }
         public float AssaultStarProgressPercent { get; set; }
+        public float EngineerCurrentRelativeScore { get; set; }
         public float EngineerStarProgressPercent { get; set; }
+        public float ReconCurrentRelativeScore { get; set; }
         public float ReconStarProgressPercent { get; set; }
+        public float SupportCurrentRelativeScore { get; set; }
         public float SupportStarProgressPercent { get; set; }
+        public float CommanderCurrentRelativeScore { get; set; }
         public float CommanderStarProgressPercent { get; set; }
         public float TimePlayedHours { get; set; }
-        public float Rounds { get; set; }
-
-        #endregion
-
-        #region Manually assigned properties
-
-        public string RankName { get; set; }
-        public float RankMaxScore { get; set; }
-        public double RankMaxTotalScore { get; set; }
+        public int Rounds { get; set; }
 
         #endregion
 
         #region Predefined properties
 
-        public float AssaultMaxScore { get; } = 155000f;
-        public float EngineerMaxScore { get; } = 131000f;
-        public float ReconMaxScore { get; } = 134000f;
-        public float SupportMaxScore { get; } = 104000f;
-        public float CommanderMaxScore { get; } = 20000f;
+        public float AssaultMaxRelativeScore { get; } = 155000f;
+        public float EngineerMaxRelativeScore { get; } = 131000f;
+        public float ReconMaxRelativeScore { get; } = 134000f;
+        public float SupportMaxRelativeScore { get; } = 104000f;
+        public float CommanderMaxRelativeScore { get; } = 20000f;
 
         #endregion
     }
